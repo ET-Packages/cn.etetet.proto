85738902bd28af114dc767605d006a43d061b9ab
tanghai <egametang@gmail.com>
2024-07-09 20:00:53 +0800

    tooleditor挪到proto包中

---
+++ Editor/ProtoEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using System.Diagnostics;
+
+namespace ET
+{
+    public static class ProtoEditor
+    {
+        [MenuItem("ET/Proto/Proto2CS")]
+        public static void Run()
+        {
+            Process process = ProcessHelper.DotNet("./Packages/cn.etetet.proto/DotNet~/Exe/ET.Proto2CS.dll", "./", true);
+
+            UnityEngine.Debug.Log(process.StandardOutput.ReadToEnd());
+        }
+        
+        public static void Init()
+        {
+            Run();
+        }
+    }
+}
